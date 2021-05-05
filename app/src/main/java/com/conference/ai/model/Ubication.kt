b07fa4e321608431b07a25ba827585437fae17af
Drake Arch <drakessn@gmail.com>
2021-05-05 18:29:56 -0500

    Issue #3 Develop of map fragment with google maps

--- app/src/main/java/com/conference/ai/model/Ubication.kt
+++ app/src/main/java/com/conference/ai/model/Ubication.kt
@@ -1,11 +1,11 @@
 package com.conference.ai.model
 
 class Ubication {
-    val name = ""
-    val address = ""
-    val latitude = ""
-    val longitude = ""
-    val phone = ""
-    val website = ""
-    val image = ""
+    val name = "Drake Arch Arts Center"
+    val address = "401 Van Ness Ave, San Francisco, CA 94102, USA"
+    val latitude = 37.77997967020105
+    val longitude = -122.42050071969913
+    val phone = "+51 942 942###"
+    val website = "https://github.com/drakessn"
+    val image = "https://sfwarmemorial.org/system/image/image/9/large_WMOH9_JoelPuliatti.jpg"
 }
--- app/src/main/java/com/conference/ai/view/ui/fragments/UbicationFragment.kt
+++ app/src/main/java/com/conference/ai/view/ui/fragments/UbicationFragment.kt
@@ -1,11 +1,23 @@
 package com.conference.ai.view.ui.fragments
 
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import com.conference.ai.R
+import com.conference.ai.model.Ubication
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,7 +29,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [UbicationFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class UbicationFragment : Fragment() {
+class UbicationFragment : Fragment(), OnMapReadyCallback {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -38,6 +50,13 @@ class UbicationFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_ubication, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+
+        mapFragment.getMapAsync(this)
+    }
+
     companion object {
         /**
          * Use this factory method to create a new instance of
@@ -56,5 +75,20 @@ class UbicationFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+        val ubication = Ubication()
+        val zoom = 16f
+        val centerMap = LatLng(ubication.latitude, ubication.longitude)
+        googleMap?.animateCamera(CameraUpdateFactory.newLatLngZoom(centerMap, zoom))
+        val centerMark = LatLng(ubication.latitude, ubication.longitude)
+        val markerOptions = MarkerOptions()
+        markerOptions.position(centerMap)
+        markerOptions.title("AIcon 2021")
+        val bitmapDraw = context?.applicationContext?.let { ContextCompat.getDrawable(it, R.drawable.aicon_logo_horizontal) } as BitmapDrawable
+        val smallMarker = Bitmap.createScaledBitmap(bitmapDraw.bitmap, 150, 150, false)
+        markerOptions.icon(BitmapDescriptorFactory.fromBitmap(smallMarker))
+        googleMap?.addMarker(markerOptions)
     }
 }
