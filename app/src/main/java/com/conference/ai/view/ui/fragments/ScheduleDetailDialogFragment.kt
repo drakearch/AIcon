d8239dc7065a905d3d046c065333c137aa2fd37c
Drake Arch <drakessn@gmail.com>
2021-05-05 09:27:53 -0500

    Issue #1 Including DialogFragments to schedule and speakers

--- app/src/main/java/com/conference/ai/view/ui/fragments/ScheduleDetailDialogFragment.kt
+++ app/src/main/java/com/conference/ai/view/ui/fragments/ScheduleDetailDialogFragment.kt
@@ -1,11 +1,17 @@
 package com.conference.ai.view.ui.fragments
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.DialogFragment
 import com.conference.ai.R
+import com.conference.ai.model.Conference
+import kotlinx.android.synthetic.main.fragment_schedule_detail_dialog.*
+import java.text.SimpleDateFormat
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,7 +23,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [ScheduleDetailDialogFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ScheduleDetailDialogFragment : Fragment() {
+class ScheduleDetailDialogFragment : DialogFragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -28,6 +34,7 @@ class ScheduleDetailDialogFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+        setStyle(STYLE_NORMAL, R.style.FullscreenDialogStyle)
     }
 
     override fun onCreateView(
@@ -36,6 +43,27 @@ class ScheduleDetailDialogFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_schedule_detail_dialog, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        toolbarConference.navigationIcon = ContextCompat.getDrawable(view.context, R.drawable.ic_close)
+        toolbarConference.setTitleTextColor(Color.WHITE)
+        toolbarConference.setNavigationOnClickListener {
+            dismiss()
+        }
+
+        val conference = arguments?.getSerializable("conference") as Conference
+        toolbarConference.title = conference.title
+
+        tvScheduleDetailTitle.text = conference.title
+        val pattern = "dd/MM/yyyy hh:mm a"
+        val simpleDF = SimpleDateFormat(pattern)
+        val date = simpleDF.format(conference.datetime)
+        tvScheduleDetailDatetime.text = date
+        tvScheduleDetailSpeaker.text = conference.speaker
+        tvScheduleDetailTopic.text = conference.topic
+        tvScheduleDetailDescription.text = conference.description
     }
 
     companion object {
@@ -56,5 +84,10 @@ class ScheduleDetailDialogFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        dialog?.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
     }
 }
--- app/src/main/java/com/conference/ai/view/ui/fragments/SpeakersDetailDialogFragment.kt
+++ app/src/main/java/com/conference/ai/view/ui/fragments/SpeakersDetailDialogFragment.kt
@@ -1,11 +1,18 @@
 package com.conference.ai.view.ui.fragments
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.DialogFragment
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.conference.ai.R
+import com.conference.ai.model.Speaker
+import kotlinx.android.synthetic.main.fragment_speakers_detail_dialog.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,7 +24,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [SpeakersDetailDialogFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class SpeakersDetailDialogFragment : Fragment() {
+class SpeakersDetailDialogFragment : DialogFragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -28,6 +35,7 @@ class SpeakersDetailDialogFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+        setStyle(STYLE_NORMAL, R.style.FullscreenDialogStyle)
     }
 
     override fun onCreateView(
@@ -36,6 +44,33 @@ class SpeakersDetailDialogFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_speakers_detail_dialog, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        toolbarSpeaker.navigationIcon = ContextCompat.getDrawable(view.context, R.drawable.ic_close)
+        toolbarSpeaker.setTitleTextColor(Color.WHITE)
+        toolbarSpeaker.setNavigationOnClickListener {
+            dismiss()
+        }
+
+        val speaker = arguments?.getSerializable("speaker") as Speaker
+        toolbarSpeaker.title = speaker.name
+
+        Glide.with(view.context)
+                .load(speaker.image)
+                .apply(RequestOptions.circleCropTransform())
+                .into(ivSpeakerDetailImage)
+        tvSpeakerDetailName.text = speaker.name
+        tvSpeakerDetailJobtitle.text = speaker.jobtitle
+        tvSpeakerDetailWorkplace.text = speaker.workplace
+        // How to put twitter?
+        tvSpeakerDetailBiography.text = speaker.biography
+    }
+
+    override fun onStart() {
+        super.onStart()
+        dialog?.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
     }
 
     companion object {
