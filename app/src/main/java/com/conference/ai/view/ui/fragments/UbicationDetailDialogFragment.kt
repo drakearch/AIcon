8ce959240c6f5bc52fe410383181fd7c094e3df9
Drake Arch <drakessn@gmail.com>
2021-05-06 08:14:09 -0500

    Issue #3 Adding map style

--- app/src/main/java/com/conference/ai/view/ui/fragments/UbicationDetailDialogFragment.kt
+++ app/src/main/java/com/conference/ai/view/ui/fragments/UbicationDetailDialogFragment.kt
@@ -1,11 +1,19 @@
 package com.conference.ai.view.ui.fragments
 
+import android.content.Intent
+import android.graphics.Color
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.DialogFragment
 import com.conference.ai.R
+import com.conference.ai.model.Ubication
+import kotlinx.android.synthetic.main.fragment_schedule_detail_dialog.*
+import kotlinx.android.synthetic.main.fragment_ubication_detail_dialog.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,7 +25,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [UbicationDetailDialogFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class UbicationDetailDialogFragment : Fragment() {
+class UbicationDetailDialogFragment : DialogFragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -28,6 +36,7 @@ class UbicationDetailDialogFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+        setStyle(STYLE_NORMAL, R.style.FullscreenDialogStyle)
     }
 
     override fun onCreateView(
@@ -36,6 +45,41 @@ class UbicationDetailDialogFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_ubication_detail_dialog, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        toolbarUbication.navigationIcon = ContextCompat.getDrawable(view.context, R.drawable.ic_close)
+        toolbarUbication.setTitleTextColor(Color.WHITE)
+        toolbarUbication.setNavigationOnClickListener {
+            dismiss()
+        }
+
+        val ubication = Ubication()
+        toolbarUbication.title = ubication.name
+        tvUbicationDetailPlace.text = ubication.name
+        tvUbicationDetailAddress.text = ubication.address
+        tvUbicationDetailPhone.text = ubication.phone
+        tvUbicationDetailWebsite.text = ubication.website
+
+        llUbicationDetailPhone.setOnClickListener {
+            val intent = Intent(Intent.ACTION_DIAL).apply {
+                data = Uri.parse("tel:${ubication.phone}")
+            }
+            startActivity(intent)
+        }
+
+        llUbicationDetailWebsite.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(ubication.website)
+            startActivity(intent)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        dialog?.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
     }
 
     companion object {
